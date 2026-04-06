f9413fd4049cae1689002096a0ed5bdd893a50bd
vinaykumarreddythangella <vinayreddyt19@gmail.com>
2026-04-06 17:01:30 -0400

    Fix BigQuery connector classpath conflict on Dataproc

--- src/main/scala/com/analytics/MainApp.scala
+++ src/main/scala/com/analytics/MainApp.scala
@@ -22,7 +22,7 @@ object MainApp {
       mode: String
   ): Unit = {
     df.write
-      .format("com.google.cloud.spark.bigquery.v2.Spark35BigQueryTableProvider")
+      .format("com.google.cloud.spark.bigquery")
       .option("table", s"$projectId:$dataset.$table")
       .option("temporaryGcsBucket", temporaryBucket)
       .mode(mode)
